b1f557fea9c9cbdc06bfd3d6efe532c672f1fa87
Alejandro Ayala <mayalaat@pichincha.com>
2022-02-08 08:50:51 -0500

    Initial commit

---
+++ lib/MainScreens/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color(0xff8041b2),
+          primaryColorDark: Color(0xffb26fe5),
+          primaryColorLight: Color(0xff4f1182),
+          accentColor: Color(0xffc681ff),
+          //backgroundColor: Color(0xffffffff),
+
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
